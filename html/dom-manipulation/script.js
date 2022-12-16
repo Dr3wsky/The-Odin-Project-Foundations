f50e13f31e8900d9e3be2b338c0f701012c010ec
Andrew McFadden <mcfadden.andrewj@gmail.com>
2022-12-16 12:12:27 -0700

    practicing DOM manipulation

--- html/dom-manipulation/script.js
+++ html/dom-manipulation/script.js
@@ -19,3 +19,38 @@ html.onclick = function() {
     this.textContent = listContent;
   }
 }
+
+
+/* New code added for TOp exercise */
+
+/* red p element properties*/
+const redP = document.createElement('p');
+redP.textContent = "Hey, I'm red!";
+redP.style.color = 'red';
+
+/* blue h3 element properties*/
+const blueH3 = document.createElement('h3');
+blueH3.textContent = "I'm a blue h3!";
+blueH3.style.color = 'blue';
+
+/* Add p and h3 elements to document, will show up below list entry box*/
+document.body.appendChild(redP);
+document.body.appendChild(blueH3);
+
+/* new div with children and properties, then append in right order */
+
+const newDiv = document.createElement('div');
+newDiv.style.cssText = "background-color:pink; padding: 10px;border: 10px solid black;"
+
+const anotherH1 = document.createElement('h1');
+anotherH1.textContent = "I'm in a div!";
+
+const anotherP = document.createElement('p');
+anotherP.textContent = "ME TOO";
+
+newDiv.appendChild(anotherH1);
+newDiv.appendChild(anotherP);
+document.body.appendChild(newDiv);
+
+
+
