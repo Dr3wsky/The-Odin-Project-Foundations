69826f762aeb1a2a74d1cc9003c01ddd7ba5b741
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-05-30 09:52:02 -0400

    linting setup woth whole odin project folder

--- javascript/events/script.js
+++ javascript/events/script.js
@@ -1,13 +1,13 @@
 function alertFunction() {
-    alert("YAY! YOU DID IT!");
-  }
+  alert("YAY! YOU DID IT!");
+}
 
-const btn2 = document.querySelector('#btn2');
+const btn2 = document.querySelector("#btn2");
 btn2.onclick = alertFunction;
 
 // const btn3 = document.querySelector('#btn3');
 // btn3.addEventListener('click', alertFunction);
 
-btn3.addEventListener('click', function (e) {
-    e.target.style.background = 'blue';
-  });
+btn3.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
